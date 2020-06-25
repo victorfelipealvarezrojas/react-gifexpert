788bc29ed9e4b68cecc66929c75e518f14ea4e0a
Mona LisaVictor <victorfelipealvarezrojas@hotmail.com>
2020-06-24 21:33:59 -0400

    docs agregar

---
+++ docs/precache-manifest.7ad2b7c4dfb894020eb4150245a33534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa4a1aefdf157fa00ffc2a81dd8a62e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cf03ea08678d50797fa",
+    "url": "/static/css/main.0098e536.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf03ea08678d50797fa",
+    "url": "/static/js/main.20f00f3c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
